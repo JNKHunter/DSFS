0a5e31f77365bab7b0ed6e7b33f860fc92d65cf6
John Hunter <john@push-it-forward.com>
2016-11-21 15:14:02 -0800

    initial commit

---
+++ code/linear_algebra.py
@@ -0,0 +1,9 @@
+def vector_add(v,w):
+	"""adds corresponding elements"""
+	return v_i + w_i for v_i, w_i in zip(v,w)]
+
+def vector_subtract(v,w):
+	"""subtracts corresponding elements"""
+	return [v_i - w_i for v_i, w_i in zip(v,w)]
+
+
