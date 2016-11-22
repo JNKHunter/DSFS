ae8249e8cd9fc5e50f0a23547ec4b9e7178655f8
John Hunter <john@push-it-forward.com>
2016-11-21 16:39:11 -0800

    added vector_sum

--- code/linear_algebra.py
+++ code/linear_algebra.py
@@ -1,9 +1,16 @@
 def vector_add(v,w):
 	"""adds corresponding elements"""
-	return v_i + w_i for v_i, w_i in zip(v,w)]
+	return [v_i + w_i for v_i, w_i in zip(v,w)]
 
 def vector_subtract(v,w):
 	"""subtracts corresponding elements"""
 	return [v_i - w_i for v_i, w_i in zip(v,w)]
 
 
+def vector_sum(vectors):
+	"""sums all corresponding elements"""
+	result = vectors[0]
+	for vector in vectors[1:]:
+		result = vector_add(result, vector)
+
+	return result
