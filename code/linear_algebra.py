79471452e9635fca80f7867e171c09931e970705
John Hunter <john@push-it-forward.com>
2016-11-22 16:04:14 -0800

    added dot product

--- code/linear_algebra.py
+++ code/linear_algebra.py
@@ -22,3 +22,11 @@ def vector_sum_reduce(vectors):
 def scalar_multiply(c,v):
 	"""c is a number, v is a vector"""
 	return [c*v_i for v_i in v]
+
+def vector_mean(vectors):
+	n = len(vectors)
+	return scalar_multiply(1/n, vector_sum(vectors))
+
+def dot(v,w):
+	"""v_1 * w_1 + ... + v_n * w_n"""
+	reteurn sum(v_i * w_i for v_i, w_i in zip(v,w))
