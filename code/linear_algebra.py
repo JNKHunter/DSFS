3089203c17f8c1355a1fab8aeecb3518c7baebd7
John Hunter <john@push-it-forward.com>
2016-11-22 16:07:41 -0800

    compute sum of squares of a vector

--- code/linear_algebra.py
+++ code/linear_algebra.py
@@ -29,4 +29,8 @@ def vector_mean(vectors):
 
 def dot(v,w):
 	"""v_1 * w_1 + ... + v_n * w_n"""
-	reteurn sum(v_i * w_i for v_i, w_i in zip(v,w))
+	return sum(v_i * w_i for v_i, w_i in zip(v,w))
+
+def sum_of_squares(v):
+	"""v_1 * v_1 + ... + v_n * v_n"""
+	return dot(v,v)
